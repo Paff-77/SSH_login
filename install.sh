6409cefbcd67257c9ff03763f353328deb27eb19
帕芙 <128116400+Paff-77@users.noreply.github.com>
2024-05-02 18:58:26 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 设置 Telegram Bot Token 和 Chat ID
+token="7068262511:AAGt13w-y2sWqgwK_KgIgm1fsAXraNMSttg"
+id="5617916510"
+
+# 创建目录 /etc/profile.d（如果不存在）
+sudo mkdir -p /etc/profile.d
+
+# 下载脚本并给予执行权限
+sudo curl -sL https://raw.githubusercontent.com/vlesstop/sshlogin/main/script.sh -o /etc/profile.d/ssh_login_alert.sh
+sudo chmod +x /etc/profile.d/ssh_login_alert.sh
+
+# 将 token 和 id 写入文件以便持久保存
+echo "$token" > ~/.ssh_login_token
+echo "$id" > ~/.ssh_login_chat_id
+
+# 运行脚本并传递参数
+/etc/profile.d/ssh_login_alert.sh "$token" "$id"
---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 从文件中读取 token 和 chat_id
+token=$(cat ~/.ssh_login_token)
+id=$(cat ~/.ssh_login_chat_id)
+
+# 获取服务器主机名
+hostname=$(hostname)
+
+# 获取登录用户信息
+login_info=$(who | awk '{print $1}')
+
+# 获取最近登录信息
+last_login=$(last -1 | awk '{print $1,$3,$4,$5,$6}')
+
+# 获取本机IP地址
+local_ip=$(curl -s ifconfig.me)
+
+# 获取被登录服务器IP地址
+login_ip=$(echo $SSH_CONNECTION | awk '{print $1}')
+
+# 拼接通知消息，包含服务器主机名、登录IP地址、登录用户信息和最近登录信息
+message="🔔 发生SSH登录！
+🖥️ 服务器：${hostname} 
+ ${local_ip}
+🔒 登录IP地址：
+(${login_ip})
+👤 登录用户信息：
+${login_info}
+📅 最近登录信息：
+${last_login}"
+
+# 发送通知消息
+curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d "chat_id=${id}&text=${message}"
